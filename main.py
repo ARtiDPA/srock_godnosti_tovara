03febd4fe65b7d16b85fb6d88cba2e9fb52d4cd8
ARtiDPA <128789278+ARtiDPA@users.noreply.github.com>
2023-05-04 00:48:48 +0400

    Добавлены сохранения
    Теперь данных записываются в текстовый документ, что позволило сохранять данные во время и после выключения программы и при последующем запуске программы не нужно вводить все заново.
    Из багов: Нельзя удалить из текстового документа

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ spisok_nazvania_tovara = []
 spisok_sroka_godnosi = []
 
 
-def timer(sekunda,minuta,chas):
+def timer(sekunda,minuta):
     while True:
         sleep(1)
         sekunda+=1
@@ -41,6 +41,11 @@ def glavnoe_menu():
             srok_godnosti = int(input("СЮДА:"))
             spisok_nazvania_tovara.append(nazvanie_tovara)
             spisok_sroka_godnosi.append(srok_godnosti)
+            file = open("Podgruzaema_pamat" , "a")
+            file.write(nazvanie_tovara + "\n")
+            file.write(str(srok_godnosti) + "\n")
+
+
         elif vvod == 3:
             for i in range(len(spisok_nazvania_tovara)):
                 print("-------------------------------------------------------------------")
@@ -50,16 +55,29 @@ def glavnoe_menu():
             nomer_index = int(input("ВВОД: "))
             spisok_nazvania_tovara.pop(nomer_index-1)
             spisok_sroka_godnosi.pop(nomer_index-1)
-            print(f"Вы успешно удалили елемент")
         else:
             print("Такого варианта нету, введите другой ответ")
 
 def start():
-    tr1 = Thread(target=timer, args=(0, 0, 0))
+    file = open("Podgruzaema_pamat", "r")
+    lines = file.read().splitlines()
+    for i in range(len(lines)):
+        if i%2 != 0:
+            print(lines[i].strip())
+            vrem_peremena = int(lines[i].strip())
+            spisok_sroka_godnosi.append(vrem_peremena)
+        elif i%2 == 0:
+            print(lines[i].strip())
+            vrem_peremena = lines[i].strip()
+            spisok_nazvania_tovara.append(vrem_peremena)
+    file.close()
+    zapusk_programma()
+
+def zapusk_programma():
+    tr1 = Thread(target=timer, args=(0, 0))
     tr2 = Thread(target=glavnoe_menu)
     tr2.start()
     tr1.start()
-
 
 if __name__=="__main__":
     start()
